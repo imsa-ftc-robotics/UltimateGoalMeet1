c5b988f630fbebe50f6d2f9b2fb86c1b16bf8608
Ethiraj Ramachandra <ethiraj4a@gmail.com>
2020-10-06 17:01:19 -0500

    added Teleop opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Default_Teleop.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.Range;
+
+public class Default_Teleop extends Robot{
+
+
+    @Override
+    public void op_mode() {
+        double speedmultiplier = 1;
+        waitForStart();
+        while (opModeIsActive()){
+            // POV Mode uses left stick for translation, and right stick to turn.
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            double strafe = gamepad1.left_stick_x;
+
+
+            double leftBackPower = Range.clip(((drive + turn - strafe)), -1.0, 1.0);
+            double rightBackPower = Range.clip(((drive - turn + strafe)), -1.0, 1.0);
+            double leftFrontPower = Range.clip(((drive + turn + strafe)), -1.0, 1.0);
+            double rightFrontPower = Range.clip(((drive - turn - strafe)), -1.0, 1.0);
+
+
+            leftBackPower *= speedmultiplier;
+            leftFrontPower *= speedmultiplier;
+            rightBackPower *= speedmultiplier;
+            rightFrontPower *= speedmultiplier;
+
+
+            leftBackDrive.setPower(leftBackPower);
+            leftFrontDrive.setPower(leftFrontPower);
+            rightBackDrive.setPower(rightBackPower);
+            rightFrontDrive.setPower(rightFrontPower);
+
+
+            telemetry.addData("Left Back Power", leftBackPower);
+            telemetry.addData("Left Front Power", leftFrontPower);
+            telemetry.addData("Right Back Power", rightBackPower);
+            telemetry.addData("Right Front Power", rightFrontPower);
+            telemetry.update();
+
+        }
+    }
+}
