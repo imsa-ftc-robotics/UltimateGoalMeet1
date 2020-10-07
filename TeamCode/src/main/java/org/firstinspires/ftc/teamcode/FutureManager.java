691413668d85b9cf1d7f9281914931804eb4bb14
Christian7573 <cag4561@gamil.com>
2020-10-06 21:43:43 -0500

    Moar work on futures
    - Javadoc on Future7573 things
    - Added FutureManager and OpMode7573 for dealing with futures
    - Added chaining futures (yet to be tested)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FutureManager.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+public class FutureManager {
+    private ArrayList<Future7573> futures = new ArrayList<>();
+
+    /**
+     * Spawns a single future for execution. FutureManager.poll() must be called for
+     * progress to happen.
+     * @param future - the future
+     */
+    public void spawn(Future7573 future) { futures.add(future); }
+
+    /**
+     * Spawns a starting future and a chain of actions behind it. The chain is advanced whenever
+     * a future advances to Future7573.Next. FutureManager.poll() must be called for progress
+     * to happen.
+     * @param initial - the initial future
+     * @param chain - additional actions that return more futures to be executed in order
+     */
+    public void spawnChain(Future7573 initial, Callable<Future7573> ... chain) {
+        ArrayList<Future7573> actual_chain = new ArrayList<>();
+        actual_chain.add(initial);
+        for (Callable<Future7573> callable : chain) {
+            actual_chain.add(new Future7573() {
+                public Future7573 poll() {
+                    try { return callable.call(); }
+                    catch (Exception e) { throw new RuntimeException(e); }
+                }
+            });
+        }
+        spawn(new Future7573.FutureChain(actual_chain));
+    }
+
+    /**
+     * Polls all futures once, allowing them to do work
+     */
+    public void poll() {
+        for (int i = 0; i < futures.size(); i++) {
+            Future7573 result = futures.get(i).poll();
+            if (result == null) {
+                futures.remove(i);
+                i--;
+            } else {
+                futures.set(i, result);
+            }
+        }
+    }
+}
